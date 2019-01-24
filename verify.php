859eb89b5dc15a984ae425dc20a970cbb64e4185
Pongsapat Timtham <35423846+golfPongsapat@users.noreply.github.com>
2019-01-24 15:52:37 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'IGP1cCGoyG8ki9iUSVs2zFOMjBrgMzXa2ba278mL7jyeDku4U9zCcElEcil+p9q13Crzu6dKzm2f3qvVOQ499g19bNuGVFXmwBTjSqCkFYt/b134CwWlmqtlx2VLmlk4lYWtXRZ8/xWj4oWg9R5I5wdB04t89/1O/w1cDnyilFU=';
+$access_token = '/m69FQoSI0aAMDrVZBQ+IWNTYPLOiriDWSnxeQSt8q3SZ7tvWqLvy0GsYVTWMv+nJaTI8tMnwcAz/IrI7fWo8c5Lkmd6fI4XiMSHss+H/7jlsgxichlmOpkQoyefLD04gDd+ubywG3xVTSvZVyOuNwdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
